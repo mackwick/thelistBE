7db74403e328a70f60ffa4c58641172a1b5e3ae3
mackwick <mackenziewicker@gmail.com>
2024-04-02 19:34:11 -0400

    update model

--- showlist/models.py
+++ showlist/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Show(models.Model):
     name=models.CharField(max_length=50)
-    type=models.CharField(max_length=20, default='tv show')
+    type=models.CharField(max_length=20, default='tv show', blank=True)
     genre=models.CharField(max_length=20, blank=True)
     recby=models.CharField(max_length=30, blank=True)
     watched=models.BooleanField(default=False)    
